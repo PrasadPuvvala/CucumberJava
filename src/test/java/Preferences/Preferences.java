31f53fed58a24c6bdb76d74c6fe42513275287ef
FSWIRAY55 <FSWIRAY55@FSWIRAY80>
2022-05-17 13:09:26 +0530

    new package added.

---
+++ src/test/java/Preferences/Preferences.java
@@ -0,0 +1,20 @@
+package Preferences;
+
+public class Preferences {
+
+	public Preferences() {
+		// TODO Auto-generated constructor stub
+	}
+	public void printText() {
+		System.out.println("function is executed");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Preferences prefObj = new Preferences();
+		prefObj.printText();
+		
+	}
+
+}
